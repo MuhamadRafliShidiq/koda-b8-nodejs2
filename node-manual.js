36d1c79497c728efeda65f362dd2fbda45a8a5e5
Muhamad Rafli Shidiq <130623661+MuhamadRafliShidiq@users.noreply.github.com>
2026-05-19 14:13:30 +0700

    Add files via upload

---
+++ node-manual.js
@@ -0,0 +1,26 @@
+// Membuat program convert tanggal dari input dd--mm--yyyy menjadi dd/mm/yyyy dengan menggunakan alur sendiri tanpa external package
+const readline = require('readline');
+const fs = require('fs');
+const moment = require('moment');
+const { type } = require('os');
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+function convertTanggal() {
+    rl.question("Masukkan Tanggal dalam format dd-mm-yyyy: ", (inputTanggal) => {
+        const validasiFormat = /^\d{2}-\d{2}-\d{4}$/.test(inputTanggal);
+        if (!validasiFormat) {
+            console.log("Format tanggal salah");
+            convertTanggal();
+        } else {
+            const hasil = inputTanggal.split("-").join("/");
+            console.log(hasil);
+            rl.close();
+        }
+    })
+}
+
+convertTanggal();
